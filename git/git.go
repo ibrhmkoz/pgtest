89bac8d0abaa1a2f6bd682c1a491a6d04d3a122c
ibrahimkoz <ibrahimkoz@outlook.com>
2024-04-08 22:48:31 +0300

    Make git.go concise

--- git/git.go
+++ git/git.go
@@ -8,11 +8,11 @@ import (
 func Root() (string, error) {
 	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
 
-	output, err := cmd.Output()
+	o, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
 
-	gitRoot := strings.TrimSpace(string(output))
-	return gitRoot, nil
+	r := strings.TrimSpace(string(o))
+	return r, nil
 }
