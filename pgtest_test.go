d295dd56acbb9e5fb466ce7c1444990c63deca80
Ronnie Smith <miniscruff@hey.com>
2024-04-09 23:55:57 -0700

    Change the Run command to return the pgxpool we establish

--- pgtest_test.go
+++ pgtest_test.go
@@ -2,7 +2,6 @@ package pgtest
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -12,65 +11,61 @@ const (
 )
 
 func TestPGTest(t *testing.T) {
-	Run(t,
-		func(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
-			// Given
-			_, err := pool.Exec(ctx, `INSERT INTO clinics (id, name, email) VALUES ($1, $2, $3)`, "test-clinic-uuid", "Test Clinic", "test@example.com")
-			require.NoError(t, err)
+	ctx := context.Background()
+	pool := Run(t, ctx, WithDesiredState(dsu))
 
-			// When
-			var name string
-			var email string
-			err = pool.QueryRow(ctx, `SELECT name, email FROM clinics`).Scan(&name, &email)
+	// Given
+	_, err := pool.Exec(ctx, `INSERT INTO clinics (id, name, email) VALUES ($1, $2, $3)`, "test-clinic-uuid", "Test Clinic", "test@example.com")
+	require.NoError(t, err)
 
-			// Then
-			require.NoError(t, err)
-			require.Equal(t, "Test Clinic", name)
-			require.Equal(t, "test@example.com", email)
-		},
-		WithDesiredState(dsu),
-	)
+	// When
+	var name string
+	var email string
+	err = pool.QueryRow(ctx, `SELECT name, email FROM clinics`).Scan(&name, &email)
+
+	// Then
+	require.NoError(t, err)
+	require.Equal(t, "Test Clinic", name)
+	require.Equal(t, "test@example.com", email)
 }
 
 func TestWithReferentialIntegrityEnabled(t *testing.T) {
-	Run(t,
-		func(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
-			_, err := pool.Exec(ctx, `INSERT INTO doctors (id, clinic_id) VALUES ($1, $2)`, "test-doctor-uuid", "non-existent-clinic-uuid")
-			require.Error(t, err)
-		},
-		WithDesiredState(dsu),
-	)
+	ctx := context.Background()
+	pool := Run(t, ctx, WithDesiredState(dsu))
+
+	_, err := pool.Exec(ctx, `INSERT INTO doctors (id, clinic_id) VALUES ($1, $2)`, "test-doctor-uuid", "non-existent-clinic-uuid")
+	require.Error(t, err)
 }
 
 func TestWithReferentialIntegrityDisabled(t *testing.T) {
-	Run(t,
-		func(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
-			_, err := pool.Exec(ctx, `INSERT INTO doctors (id, clinic_id) VALUES ($1, $2)`, "test-doctor-uuid", "non-existent-clinic-uuid")
-			require.NoError(t, err)
-		},
+	ctx := context.Background()
+	pool := Run(t, ctx,
 		WithReferentialIntegrityDisabled(),
 		WithDesiredState(dsu),
 	)
+
+	_, err := pool.Exec(ctx, `INSERT INTO doctors (id, clinic_id) VALUES ($1, $2)`, "test-doctor-uuid", "non-existent-clinic-uuid")
+	require.NoError(t, err)
 }
 
 func TestWithVersion(t *testing.T) {
-	Run(t,
-		func(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
-			// Given
-			_, err := pool.Exec(ctx, `INSERT INTO clinics (id, name, email) VALUES ($1, $2, $3)`, "test-clinic-uuid", "Test Clinic", "test@example.com")
-			require.NoError(t, err)
-
-			// When
-			var name string
-			var email string
-			err = pool.QueryRow(ctx, `SELECT name, email FROM clinics`).Scan(&name, &email)
-
-			// Then
-			require.NoError(t, err)
-			require.Equal(t, "Test Clinic", name)
-			require.Equal(t, "test@example.com", email)
-		},
+	ctx := context.Background()
+	pool := Run(t, ctx,
 		WithVersion("14"),
 		WithDesiredState(dsu),
 	)
+
+	// Given
+	_, err := pool.Exec(ctx, `INSERT INTO clinics (id, name, email) VALUES ($1, $2, $3)`, "test-clinic-uuid", "Test Clinic", "test@example.com")
+	require.NoError(t, err)
+
+	// When
+	var name string
+	var email string
+	err = pool.QueryRow(ctx, `SELECT name, email FROM clinics`).Scan(&name, &email)
+
+	// Then
+	require.NoError(t, err)
+	require.Equal(t, "Test Clinic", name)
+	require.Equal(t, "test@example.com", email)
 }
